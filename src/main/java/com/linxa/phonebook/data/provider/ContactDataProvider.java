b4f4834561f522ed676153c03c4e980e535c0ceb
amin-linxa <amin.hosseini@linxa.com>
2023-04-19 17:41:44 +0400

    Vaadin CRUD + JDBC + MySQL

--- src/main/java/com/linxa/phonebook/data/provider/ContactDataProvider.java
+++ src/main/java/com/linxa/phonebook/data/provider/ContactDataProvider.java
@@ -1,102 +1,166 @@
 package com.linxa.phonebook.data.provider;
 
 import com.linxa.phonebook.data.entity.Contact;
+import com.linxa.phonebook.properties.DbProperties;
 import com.vaadin.flow.component.crud.CrudFilter;
 import com.vaadin.flow.data.provider.AbstractBackEndDataProvider;
 import com.vaadin.flow.data.provider.Query;
 import com.vaadin.flow.data.provider.SortDirection;
 
-import java.lang.reflect.Field;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
-
-import static java.util.Comparator.naturalOrder;
 
 public class ContactDataProvider extends AbstractBackEndDataProvider<Contact, CrudFilter> {
 
-    private final List<Contact> DATABASE = new ArrayList<>();
+    private static final String SQL_INSERT =
+        "     insert into contact (first_name, last_name, email, phone_number, country, city, street)" +
+            " values (?, ?, ?, ?, ?, ?, ?)";
 
-    private static Predicate<Contact> predicate(CrudFilter filter) {
-        return filter.getConstraints().entrySet().stream().map(constraint -> (Predicate<Contact>) contact -> {
-            try {
-                Object value = valueOf(constraint.getKey(), contact);
-                return value != null && value.toString().toLowerCase().contains(constraint.getValue().toLowerCase());
-            } catch (Exception e) {
-                e.printStackTrace();
-                return false;
-            }
-        }).reduce(Predicate::and).orElse(e -> true);
-    }
+    private static final String SQL_UPDATE =
+        "     update contact" +
+            " set first_name = ?, last_name = ?, email = ?, phone_number = ?, country = ?, city = ?, street = ?" +
+            " where id = ?";
 
-    private static Comparator<Contact> comparator(CrudFilter filter) {
-        return filter.getSortOrders().entrySet().stream().map(sortClause -> {
-            try {
-                Comparator<Contact> comparator = Comparator.comparing(contact -> (Comparable) valueOf(sortClause.getKey(), contact));
-                if (sortClause.getValue() == SortDirection.DESCENDING) {
-                    comparator = comparator.reversed();
-                }
-                return comparator;
-            } catch (Exception ex) {
-                return (Comparator<Contact>) (o1, o2) -> 0;
-            }
-        }).reduce(Comparator::thenComparing).orElse((o1, o2) -> 0);
-    }
+    private static final String SQL_DELETE = "delete from contact where id = ?";
 
-    private static Object valueOf(String fieldName, Contact contact) {
-        try {
-            Field field = Contact.class.getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get(contact);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
+    private static final Map<String, String> MAP_FIELD_TO_COLUMN = Map.of(
+        "firstName", "first_name",
+        "lastName", "last_name",
+        "email", "email",
+        "phoneNumber", "phone_number");
+
+    public boolean isUniquePhoneNumber(Long currentContactId, String phoneNumber) {
+        var dbProperties = DbProperties.getInstance();
+        try (var connection = DriverManager.getConnection(dbProperties.getConnectionUrl(), dbProperties.getUsername(), dbProperties.getPassword())) {
+            var whereClause = String.format(" where phone_number = '%s'", phoneNumber);
+            if (Objects.nonNull(currentContactId))
+                whereClause += String.format(" and id <> %d", currentContactId);
+            var statement = connection.prepareStatement("select count(id) = 0 from contact " + whereClause);
+            var resultSet = statement.executeQuery();
+            resultSet.next();
+            return resultSet.getBoolean(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
-    public boolean isUniquePhoneNumber(Long currentContactId, String phoneNumber) {
-        Stream<Contact> stream = DATABASE.stream();
-        if (Objects.nonNull(currentContactId))
-            stream = stream.filter(contact -> !Objects.equals(contact.getId(), currentContactId));
-        return stream.noneMatch(contact -> Objects.equals(contact.getPhoneNumber(), phoneNumber));
-    }
-
     public void persist(Contact item) {
-        if (Objects.isNull(item.getId())) item.setId(DATABASE.stream().map(Contact::getId).max(naturalOrder()).orElse(0L) + 1);
-
-        find(item.getId()).ifPresentOrElse(contact -> {
-            int position = DATABASE.indexOf(contact);
-            DATABASE.remove(contact);
-            DATABASE.add(position, item);
-        }, () -> DATABASE.add(item));
+        var dbProperties = DbProperties.getInstance();
+        try (var connection = DriverManager.getConnection(dbProperties.getConnectionUrl(), dbProperties.getUsername(), dbProperties.getPassword())) {
+            var id = item.getId();
+            var statement = Objects.isNull(id) ? connection.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS) : connection.prepareStatement(SQL_UPDATE);
+            statement.setString(1, item.getFirstName());
+            statement.setString(2, item.getLastName());
+            statement.setString(3, item.getEmail());
+            statement.setString(4, item.getPhoneNumber());
+            statement.setString(5, item.getCountry());
+            statement.setString(6, item.getCity());
+            statement.setString(7, item.getStreet());
+            if (Objects.nonNull(id))
+                statement.setLong(8, item.getId());
+            statement.executeUpdate();
+            if (Objects.isNull(id)) {
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next())
+                        item.setId(generatedKeys.getLong(1));
+                    else
+                        throw new SQLException("Creating contact failed, no ID obtained.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void delete(Contact item) {
-        DATABASE.removeIf(entity -> Objects.equals(entity.getId(), item.getId()));
+        var dbProperties = DbProperties.getInstance();
+        try (var connection = DriverManager.getConnection(dbProperties.getConnectionUrl(), dbProperties.getUsername(), dbProperties.getPassword())) {
+            var statement = connection.prepareStatement(SQL_DELETE);
+            statement.setLong(1, item.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected Stream<Contact> fetchFromBackEnd(Query<Contact, CrudFilter> query) {
-        int offset = query.getOffset();
-        int limit = query.getLimit();
-
-        Stream<Contact> stream = DATABASE.stream();
-
-        if (query.getFilter().isPresent()) stream = stream.filter(predicate(query.getFilter().get())).sorted(comparator(query.getFilter().get()));
-
-        return stream.skip(offset).limit(limit);
+        var dbProperties = DbProperties.getInstance();
+        try (var connection = DriverManager.getConnection(dbProperties.getConnectionUrl(), dbProperties.getUsername(), dbProperties.getPassword())) {
+            var whereClause = query.getFilter().isPresent() ? fetchWhereClause(query.getFilter().get()) : "";
+            var orderClause = query.getFilter().isPresent() ? fetchOrderClause(query.getFilter().get()) : "";
+            var statement = connection.prepareStatement("select * from contact " + whereClause + " " + orderClause + " limit ?, ?");
+            statement.setInt(1, query.getOffset());
+            statement.setInt(2, query.getLimit());
+            var resultSet = statement.executeQuery();
+            var contacts = new ArrayList<Contact>();
+            while (resultSet.next())
+                contacts.add(extractContact(resultSet));
+            return contacts.stream();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Stream.empty();
+        }
     }
 
     @Override
     protected int sizeInBackEnd(Query<Contact, CrudFilter> query) {
-        return (int) fetchFromBackEnd(query).count();
+        var dbProperties = DbProperties.getInstance();
+        try (var connection = DriverManager.getConnection(dbProperties.getConnectionUrl(), dbProperties.getUsername(), dbProperties.getPassword())) {
+            var whereClause = query.getFilter().isPresent() ? fetchWhereClause(query.getFilter().get()) : "";
+            var orderClause = query.getFilter().isPresent() ? fetchOrderClause(query.getFilter().get()) : "";
+            var statement = connection.prepareStatement("select count(id) as count from contact " + whereClause + " " + orderClause + " limit ?, ?");
+            statement.setInt(1, query.getOffset());
+            statement.setInt(2, query.getLimit());
+            var resultSet = statement.executeQuery();
+            resultSet.next();
+            return resultSet.getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
 
-    private Optional<Contact> find(Long id) {
-        return DATABASE.stream().filter(entity -> Objects.equals(entity.getId(), id)).findFirst();
+    private String fetchWhereClause(CrudFilter filter) {
+        if (filter.getConstraints().isEmpty())
+            return "";
+        StringBuilder where = new StringBuilder(" where ");
+        filter.getConstraints().forEach((field, term) ->
+            where.append("lower(").append(MAP_FIELD_TO_COLUMN.get(field)).append(")")
+                .append(" like lower('%").append(term.replaceAll("%", "\\\\%")).append("%')")
+                .append(" and "));
+        return where.delete(where.length() - 5, where.length()).toString();
+    }
+
+    private String fetchOrderClause(CrudFilter filter) {
+        if (filter.getSortOrders().isEmpty())
+            return "";
+        StringBuilder order = new StringBuilder(" order by ");
+        filter.getSortOrders().forEach((field, direction) ->
+            order.append(MAP_FIELD_TO_COLUMN.get(field)).append(" ")
+                .append(Objects.equals(direction, SortDirection.ASCENDING) ? "ASC" : "DESC")
+                .append(", "));
+        return order.delete(order.length() - 2, order.length()).toString();
+    }
+
+    private Contact extractContact(ResultSet resultSet) throws SQLException {
+        return new Contact(
+            resultSet.getLong("id"),
+            resultSet.getString("first_name"),
+            resultSet.getString("last_name"),
+            resultSet.getString("email"),
+            resultSet.getString("phone_number"),
+            resultSet.getString("country"),
+            resultSet.getString("city"),
+            resultSet.getString("street")
+        );
     }
 
 }
